014b47063404b291f5d457a22f579de28cdc4eeb
Coursera Learner <nobody@nowhere.nul>
2020-07-14 14:18:45 +0000

    Did assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -3,21 +3,19 @@
 #include <ctype.h>
 //decrypt returns the value of key
 //frequency_count returns the character with the highest frequency
-int decrypt(FILE *f)
-{
-  int c;
-  int letters[26];
-  for(int z=0;z<26;z++)
-    letters[z]=0;
-  while(c=fgetc(f)!=EOF)
+int decrypt(FILE *f){
+  int letters[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int  c;
+  while((c=fgetc(f))!=EOF)
     {
       if(isalpha(c)){
 	c=tolower(c);
 	c-='a';
-	letters[c]++;
+	int ch=(int) c;
+	letters[ch]++;
       }}
   int max=0;
-  int pos=0;
+  int pos=-1;
   for(int i=0;i<26;i++)
     {
       if(letters[i]>max)
@@ -25,11 +23,13 @@ int decrypt(FILE *f)
 	  max=letters[i];
 	  pos=i;
 	}}
-  int k=0;
-  if(pos>=('e'-'a')) k=pos-('e'-'a');
-  else
-    k=(26-('e'-'a'))+pos;
-  return k;
+  int r=pos;
+  char e='e';
+  int asci=(int) e;
+  int diff=r-asci+97;
+  if(diff<0)
+    diff+=26;
+  return r;
 }
 int main(int argc, char ** argv){
   if(argc!=2){
@@ -42,12 +42,14 @@ int main(int argc, char ** argv){
     return EXIT_FAILURE;
   }
   else{
-  int key=decrypt(f);
+    int key=decrypt(f);
     printf("%d",key);
-  if(fclose(f)!=0){
-    perror("Failed to close the input file\n");
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
+    if(fclose(f)!=0){
+      perror("Failed to close the input file\n");
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
   }}
   
+  
+
