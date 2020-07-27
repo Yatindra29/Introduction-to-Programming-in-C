23f0e0ef98eb04facc6deb596415c44e5f78678e
Coursera Learner <nobody@nowhere.nul>
2020-07-27 19:20:02 +0000

    DId assignmert 32

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -6,6 +6,15 @@
 
 char *get_key(char * text);
 char *get_value(char *text);
+kvpair_t *to_ret(char *text);
+ kvpair_t *to_ret(char *text){
+ kvpair_t *t;
+ t=malloc(sizeof(*t));
+ (*t).key=get_key(text);
+ (*t).value=get_value(text);
+ return t;
+ free(t);
+}
 kvarray_t * readKVs(const char * fname) {
   //WRITE ME
   FILE *f=fopen(fname,"r");
@@ -13,33 +22,32 @@ kvarray_t * readKVs(const char * fname) {
     fprintf(stderr,"Failed to open file");
   }
   kvarray_t *to_return=malloc(sizeof(*to_return));
+  (*to_return).clear=NULL;
   (*to_return).arr=NULL;
   (*to_return).size=0;
-  // kvpair_t *to_fill=malloc(sizeof(*to_fill));
-  //(*to_fill).key=NULL;
-  //(*to_fill).value=NULL;
   char *read;
   read=NULL;
   size_t sz=0;
-  // kvpair_t *to_fill=NULL;
+   kvpair_t *to_fill=NULL;
   while(getline(&read,&sz,f)>=0){
-    kvpair_t *to_fill=malloc(sizeof(*to_fill));
-    ((*to_fill).key)=get_key(read);
-    ((*to_fill).value)=get_value(read);
-    ((*to_return).arr)=realloc(((*to_return).arr),(((*to_return).size)+1) * sizeof((*to_return).arr));
+     to_fill=to_ret(read);
+     ((*to_return).arr)=realloc(((*to_return).arr),(((*to_return).size)+1) * sizeof((*to_return).arr));
     (*to_return).arr[(*to_return).size]=to_fill;
+    ((*to_return).clear)=realloc(((*to_return).clear),(((*to_return).size)+1) * sizeof((*to_return).clear));
+    ((*to_return).clear)[(*to_return).size]=read;
     ((*to_return).size)++;
-    read=NULL;
-    //(*to_fill).key=NULL;
-    //(*to_fill).value=NULL;
+    // free(read);
+      read=NULL;
   }
   free(read);
+  read=NULL;
   int res=fclose(f);
   if(res!=0){
     perror("Failed to close the file");
     return NULL;
   }
   return to_return;
+  free(to_return);
 }
 char *get_key(char * text){
   char *to_return=NULL;
@@ -53,9 +61,6 @@ char *get_key(char * text){
   to_return=realloc(to_return, (size+1)*sizeof(to_return));
   to_return[size]='\0';
   return to_return;
-  // for(int i=0;i<size;i++){
-  // free(to_return[i]);
-  // }
    free(to_return);
 }
 char *get_value(char *text){
@@ -69,19 +74,12 @@ char *get_value(char *text){
 }
 void freeKVs(kvarray_t * pairs) {
   //WRITE ME
-  // kvpair_t *curr=NULL;
   for(int i=0;i<(*pairs).size;i++){
-    // curr=(*pairs).arr[i];
-    // free((*curr).key);
-    // free((*curr).value);
-    // free(curr);
-    // }
-    // free((*pairs).arr);
-      // free((*pairs).arr[i]);
-    // free(pairs);
-    free(pairs -> arr[i] -> key);
+    free(pairs -> clear[i]);
+     free(pairs -> arr[i] -> key);
     free(pairs -> arr[i]);
   }
+  free(pairs -> clear);
   free(pairs -> arr);
   free(pairs);
 }
