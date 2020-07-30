e988e5cb4a0d1c44ec1f19548fbc05bb00419ad5
Coursera Learner <nobody@nowhere.nul>
2020-07-30 14:05:55 +0000

    Main.c

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -7,17 +7,18 @@
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   //WRITE ME
-  counts_t *ans;
-  ans=createCounts();
+  counts_t *ans=createCounts();
   FILE *f=fopen(filename,"r");
-  if(f==NULL)
+  if(f==NULL){
   return NULL;
+  }
   char *line=NULL;
   size_t sz=0;
-  while((getline(&line,&sz,f))>=0){
+  while(getline(&line,&sz,f)>=0){
     char *n=strchr(line,'\n');
-    if(n!=NULL)
+    if(n!=NULL){
       *n='\0';
+    }
     char *x=lookupValue(kvPairs, line);
     addCount(ans,x);
       }
@@ -36,8 +37,7 @@ int main(int argc, char ** argv) {
   }
   //read the key/value pairs from the file named by argv[1] (call the result kv)
   // FILE *f=fopen(argv[1],"r");
-  kvarray_t *kv;
-  kv=readKVs(argv[1]);
+  kvarray_t *kv=readKVs(argv[1]);
  //count from 2 to argc (call the number you count i)
   for(int i=2;i<argc;i++){ 
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
@@ -52,9 +52,10 @@ int main(int argc, char ** argv) {
     printCounts(c,f1);
     //print the counts from c into the FILE f
     int res=fclose(f1);
-    if(res!=0)
+    if(res!=0){
       return EXIT_FAILURE;
     //close f
+    }
     free(outName);
     freeCounts(c);
     //free the memory for outName and c
