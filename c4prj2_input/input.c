f15aa1f6f00257aff9f2f7ae37a56ea59eb01657
Coursera Learner <nobody@nowhere.nul>
2020-08-04 19:26:12 +0000

    changed input.c

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -9,24 +9,21 @@
 #include "input.h"
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
-  deck_t **arr_of_deck=malloc(sizeof(**arr_of_deck));
-  deck_t *deck=malloc(sizeof(*deck));
-  deck->cards=NULL;
-  deck->n_cards=0;
+  deck_t **arr_of_deck=NULL;
   size_t sz=0;
   char *line=NULL;
   int n=0;
   while(getline(&line,&sz,f)>=0){
-    deck=hand_from_string(line,fc);
-    if(deck!=NULL){
-      arr_of_deck=realloc(arr_of_deck, (n+1)*sizeof(**arr_of_deck));
+    deck_t *deck=hand_from_string(line,fc);
+    arr_of_deck=realloc(arr_of_deck, (n+1)*sizeof(*arr_of_deck));
+    if(deck==NULL)
+      continue;
     arr_of_deck[n]=deck;
     n++;
-    }
-    else{
-      fprintf(stderr,"A hand consisted of less than 5 cards");
-      return NULL;
-    }
+      //fprintf(stderr,"A hand consisted of less than 5 cards");
+      //return NULL;
+      //continue;
+      //}
   }
   free(line);
   *n_hands=n;
@@ -34,7 +31,6 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
 }
 deck_t * hand_from_string(const char * str, future_cards_t * fc){
   int len=strlen(str);
-  int count=0;
   deck_t *to_return=malloc(sizeof(*to_return));
   to_return->cards=NULL;
   to_return->n_cards=0;
@@ -42,19 +38,18 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
     if(isdigit(str[i]) && isalpha(str[i+1])){
 	card_t card=card_from_letters(str[i],str[i+1]);
 	add_card_to(to_return,card);
-	count++;
+	i++;
       }
     else if(str[i]=='?' && isdigit(str[i+1])){
       card_t *em=add_empty_card(to_return);
-      add_future_card(fc,str[i+1],em);
-      count++;
+      int index=str[i+1]-'0';
+      add_future_card(fc,index,em);
+      i++;
     }
-    if(str[i]==';'){
-      if(count<5){
+      if(to_return->n_cards<5){
 	fprintf(stderr,"Less than 5 cards");
 	return NULL;
       }
-      count=0;
-    }}
+  }
   return to_return;
 }
