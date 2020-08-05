1fc5e30efabcf9b79668a915b25d9ea4f8f3ff5f
Coursera Learner <nobody@nowhere.nul>
2020-08-05 09:02:29 +0000

    changed input.c

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -56,7 +56,7 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
         add_future_card(fc, atoi(num), add_empty_card(to_return));
     }}
       if(to_return->n_cards<5){
-	fprintf(stderr,"Less than 5 cards");
+	//fprintf(stderr,"Less than 5 cards");
 	return NULL;
       }
   return to_return;
