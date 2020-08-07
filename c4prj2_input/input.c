f991c0374fa2a974c11213642173ebac7c8f03db
Coursera Learner <nobody@nowhere.nul>
2020-08-07 14:15:30 +0000

    changed input.c

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -13,9 +13,12 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   size_t sz=0;
   char *line=NULL;
   size_t n=0;
+  deck_t *deck=malloc(sizeof(deck));
+  deck->cards=NULL;
+  deck->n_cards=0;
   while(getline(&line,&sz,f)>=0){
-    deck_t *deck=hand_from_string(line,fc);
-    arr_of_deck=realloc(arr_of_deck, (n+1)*sizeof(*arr_of_deck));
+    deck=hand_from_string(line,fc);
+    arr_of_deck=realloc(arr_of_deck, (n+1)*sizeof(*deck));
     if(deck==NULL)
       continue;
     arr_of_deck[n]=deck;
